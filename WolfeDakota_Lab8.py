eedb3538e07f21686f88eafc310b9805cff51c25
asherra4 <150079509+asherra4@users.noreply.github.com>
2023-11-06 16:05:06 -0500

    Ashton_Nov6_WolfeDakota_Lab8.py

--- WolfeDakota_Lab8.py
+++ WolfeDakota_Lab8.py
@@ -1,2 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def diffusion_ftcs(nspace, ntime, tau_rel, args)
+  L, kappa = args
+
+  h = L/(nspace-1) 
+  t_sigma = h**2/2*kappa
+  tau = t_sigma*tau_rel # tau_rel = tau / t_sigma
+  coeff = kappa*tau/h**2
+  if coeff < 0.5 :
+      print('Solution is expected to be stable')
+  else:
+      print('WARNING: Solution is expected to be unstable')
